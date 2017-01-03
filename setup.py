3af3f3ddb64662bae2b4084e46d88b3a39348eae
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-01-03 22:50:28 +0100

    List requirements in setup file

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from setuptools import setup, find_packages
 
 
-with open('requirements.txt') as f:
-    requires = f.read().strip().split('\n')
-
-with open('dependency_links.txt') as f:
-    links = f.read().strip().split('\n')
-
 setup(
     name='flask-projectile',
     url='https://github.com/Vayel/flask-projectile',
     author='Vincent Lefoulon',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=requires,
-    dependency_links=links,
+    install_requires=[
+        'Flask',
+        'Flask-WTF',
+        'projectile',
+        'javelot',
+    ],
+    dependency_links=[
+        'git+https://github.com/Vayel/projectile#egg=projectile',
+        'git+https://github.com/Vayel/javelot#egg=javelot',
+    ],
 )
