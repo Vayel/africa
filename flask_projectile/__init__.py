e13f06d538d9e23f76c4722c5cdda0ec0d10e80b
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-01-04 15:52:42 +0100

    Make instance folder optional

--- flask_projectile/__init__.py
+++ flask_projectile/__init__.py
@@ -8,8 +8,11 @@ app = Flask(__name__, instance_relative_config=True)
 # Load the default configuration
 app.config.from_object('config.default')
 
-# Load the configuration from the instance folder
-app.config.from_pyfile('config.py')
+# Tru to load the configuration from the instance folder
+try:
+    app.config.from_pyfile('config.py')
+except FileNotFoundError:
+    pass
 
 # Load the file specified by the APP_CONFIG_FILE environment variable
 # Variables defined here will override those in the default configuration
